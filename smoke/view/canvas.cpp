05f52c5a5541d986e0b542525225774275cebaaa
Laura Baakman <laurabaakman@gmail.com>
2017-02-10 08:57:42 +0100

    Fixed resizeGL signature, it is now called when the window resizes.

--- smoke/view/canvas.cpp
+++ smoke/view/canvas.cpp
@@ -114,8 +114,21 @@ void Canvas::paintGL()
     this->shaderProgram->release();
 }
 
-void Canvas::resizeGL(){
+void Canvas::resizeGL(int w, int h)
+{
     qDebug() << "resizeGL" << &endl;
+
+//    // Calculate aspect ratio
+//    qreal aspect = qreal(w) / qreal(h ? h : 1);
+
+//    // Set near plane to 3.0, far plane to 7.0, field of view 45 degrees
+//    const qreal zNear = 3.0, zFar = 7.0, fov = 45.0;
+
+//    // Reset projection
+//    projection.setToIdentity();
+
+//    // Set perspective projection
+//    projection.perspective(fov, aspect, zNear, zFar);
 }
 
 void Canvas::constructModelViewProjectionMatrix()
--- smoke/view/canvas.h
+++ smoke/view/canvas.h
@@ -27,7 +27,7 @@ public slots:
 protected:
     void initializeGL() Q_DECL_OVERRIDE;
     void paintGL() Q_DECL_OVERRIDE;
-    void resizeGL() Q_DECL_OVERRIDE;
+    void resizeGL(int w, int h) Q_DECL_OVERRIDE;
 
 private:
     //Shaders
