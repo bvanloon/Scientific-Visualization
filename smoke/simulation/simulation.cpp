f0f77e4536cb7ff5ddf2a8948cff39eccb931830
Laura Baakman <laurabaakman@gmail.com>
2017-02-10 11:16:30 +0100

    New simulation class.

---
+++ smoke/simulation/simulation.cpp
@@ -0,0 +1,13 @@
+#include "simulation.h"
+
+Simulation::Simulation(QObject *parent) : QObject(parent)
+{
+//    vertices.append(QVector3D(-0.8f, -0.8f, 0.0f));
+//    vertices.append(QVector3D(0.8f, -0.8f, 0.0f));
+//    vertices.append(QVector3D(0.0f, 0.8f, 0.0f));
+
+//    colors.append(QVector3D(1.0f, 0.0f, 0.0f));
+//    colors.append(QVector3D(0.0f, 1.0f, 0.0f));
+//    colors.append(QVector3D(0.0f, 0.0f, 1.0f));
+}
+
