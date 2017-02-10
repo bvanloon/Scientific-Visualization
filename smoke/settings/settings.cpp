f2f0fc4394bc32567c265b2c417111c1e5876aee
Laura Baakman <laurabaakman@gmail.com>
2017-02-10 15:39:57 +0100

    Overloaded stream operators for settings object.

--- smoke/settings/settings.cpp
+++ smoke/settings/settings.cpp
@@ -12,3 +12,33 @@ Settings::~Settings()
     delete simulation;
 }
 
+QDebug Settings::operator<<(QDebug stream, const Settings &settings)
+{
+    stream << "Settings [\n\t"
+           << settings.canvas
+           << "\n\t"
+           << settings.simulation
+           << "]" << &endl;
+
+    return stream;
+}
+
+
+QDebug Settings::Simulation::operator<<(QDebug stream, const Settings::Simulation &simulation)
+{
+    stream << "Simulation [\n\t"
+           << "dimension: " << simulation.dimension
+           << "]" << &endl;
+
+    return stream;
+}
+
+QDebug Settings::Canvas::operator<<(QDebug stream, const Settings::Canvas &canvas)
+{
+    stream << "Canvas [\n\t"
+           << "width: " << canvas.width
+           << "height: " << canvas.height
+           << "]" << &endl;
+
+    return stream;
+}
--- smoke/settings/settings.h
+++ smoke/settings/settings.h
@@ -10,9 +10,14 @@ public:
     explicit Settings(QObject *parent = 0);
     ~Settings();
 
+    friend QDebug operator<<(QDebug stream, const Settings &settings);
+
     class Simulation {
         public:
             int dimension;
+
+            friend QDebug operator<<(QDebug stream, const Settings::Simulation &simulation);
+
             Simulation() :
                 dimension(50)
             {}
@@ -23,6 +28,9 @@ public:
         public:
             int width;
             int height;
+
+            friend QDebug operator<<(QDebug stream, const Settings::Canvas &canvas);
+
             Canvas() :
                 width(-1),
                 height(-1)
