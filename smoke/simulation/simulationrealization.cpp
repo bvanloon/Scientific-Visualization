9f14d5f36e414d44596796a88e8f3282c2474ada
Laura Baakman <laurabaakman@gmail.com>
2017-02-10 14:11:44 +0100

    Copied parameters.

--- smoke/simulation/simulationrealization.cpp
+++ smoke/simulation/simulationrealization.cpp
@@ -1,6 +1,10 @@
 #include "simulationrealization.h"
 
-simulationRealization::simulationRealization()
+simulationRealization::simulationRealization():
+    DIM(50), dt(0.4), visc(0.001),
+    color_dir(0), vec_scale(1000), draw_smoke(0), draw_vecs(1),
+    COLOR_BLACKWHITE(0), COLOR_RAINBOW(1), COLOR_BANDS(2),
+    scalar_col(0), frozen(0)
 {
 
 }
--- smoke/simulation/simulationrealization.h
+++ smoke/simulation/simulationrealization.h
@@ -1,11 +1,37 @@
 #ifndef SIMULATIONREALIZATION_H
 #define SIMULATIONREALIZATION_H
 
+#include "rfftw.h"
+#include <math.h>
 
 class simulationRealization
 {
 public:
     simulationRealization();
+
+private:
+    //--- SIMULATION PARAMETERS ------------------------------------------------------------------------
+    const int DIM;				//size of simulation grid
+    double dt;				//simulation time step
+    float visc;				//fluid viscosity
+    fftw_real *vx, *vy;             //(vx,vy)   = velocity field at the current moment
+    fftw_real *vx0, *vy0;           //(vx0,vy0) = velocity field at the previous moment
+    fftw_real *fx, *fy;	            //(fx,fy)   = user-controlled simulation forces, steered with the mouse
+    fftw_real *rho, *rho0;			//smoke density at the current (rho) and previous (rho0) moment
+    rfftwnd_plan plan_rc, plan_cr;  //simulation domain discretization
+
+
+    //--- VISUALIZATION PARAMETERS ---------------------------------------------------------------------
+    int   winWidth, winHeight;      //size of the graphics window, in pixels
+    int   color_dir;            //use direction color-coding or not
+    float vec_scale;			//scaling of hedgehogs
+    int   draw_smoke;           //draw the smoke or not
+    int   draw_vecs;            //draw the vector field or not
+    const int COLOR_BLACKWHITE;   //different types of color mapping: black-and-white, rainbow, banded
+    const int COLOR_RAINBOW;
+    const int COLOR_BANDS;
+    int   scalar_col;           //method for scalar coloring
+    int   frozen;               //toggles on/off the animation
 };
 
 #endif // SIMULATIONREALIZATION_H
