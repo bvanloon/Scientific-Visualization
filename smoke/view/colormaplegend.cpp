c2849ac6e10661139567a7eab05fd1d7bcaf3d85
Laura Baakman <laurabaakman@gmail.com>
2017-02-16 20:54:38 +0100

    Draw the image of the colormap and resize it when the window resizes.

--- smoke/view/colormaplegend.cpp
+++ smoke/view/colormaplegend.cpp
@@ -1,14 +1,38 @@
 #include "colormaplegend.h"
 #include "ui_colormaplegend.h"
 
+#include <QDebug>
+
+const int ColorMapLegend::colorMapImageWidth = 10;
+
 ColorMapLegend::ColorMapLegend(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::ColorMapLegend)
+    ui(new Ui::ColorMapLegend),
+    colorMap(new RainbowColorMap(255))
 {
     ui->setupUi(this);
+
+    draw();
 }
 
 ColorMapLegend::~ColorMapLegend()
 {
     delete ui;
+    delete colorMap;
+}
+
+void ColorMapLegend::resizeEvent(QResizeEvent *event)
+{
+    draw();
+}
+
+void ColorMapLegend::draw()
+{
+    drawColorMapImage();
+}
+
+void ColorMapLegend::drawColorMapImage()
+{
+    QImage scaledImage = colorMap->scaled(colorMapImageWidth, this->height());
+    this->ui->colorMapImage->setPixmap(QPixmap::fromImage(scaledImage));
 }
