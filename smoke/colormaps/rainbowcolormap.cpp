7a97a086c4a10323ac07f86d31397869c30f9df6
Laura Baakman <laurabaakman@gmail.com>
2017-02-16 19:44:18 +0100

    Multiply with the correct factor and differentiate between the index and f.

--- smoke/colormaps/rainbowcolormap.cpp
+++ smoke/colormaps/rainbowcolormap.cpp
@@ -4,11 +4,12 @@
 
 #include <QDebug>
 #include <QImage>
+#include <QColor>
 
 RainbowColorMap::RainbowColorMap(int numColors, float dx):
     AbstractColorMap(numColors),
     dx(dx)
-{
+{ 
     fillImage();
 }
 
@@ -20,17 +21,18 @@ QImage* RainbowColorMap::getImage()
 void RainbowColorMap::fillImage()
 {
     float stepSize = 1.0/(numColors - 1);
-    for(float i = 0; i < numColors; i+=stepSize){
-        this->image->setPixel(0, i, toRainbowColor(i));
+    float f = 0;
+    for(int i = 0; i < numColors; f+=stepSize, i++){
+        this->image->setPixel(0, i, toRainbowColor(f));
     }
 }
 
 QRgb RainbowColorMap::toRainbowColor(float f)
 {
     float g = (6 - 2 * dx) * f + dx;
-    int red = fmax(0, (3 - fabs(g - 4) - fabs(g - 5)) / 2.0) * 256;
-    int green = fmax(0, (4 - fabs(g - 2) - fabs(g - 4)) / 2.0) * 256;
-    int blue = fmax(0, (3 - fabs(g - 1) - fabs(g - 2)) / 2.0) * 256;
+    int red = fmax(0, (3 - fabs(g - 4) - fabs(g - 5)) / 2.0) * 255;
+    int green = fmax(0, (4 - fabs(g - 2) - fabs(g - 4)) / 2.0) * 255;
+    int blue = fmax(0, (3 - fabs(g - 1) - fabs(g - 2)) / 2.0) * 255;
     return qRgb(red, green, blue);
 }
 
