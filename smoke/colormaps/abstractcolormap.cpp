4ac866b74c977fc8e1b7e8f991a7d3d85af56f18
Laura Baakman <laurabaakman@gmail.com>
2017-02-16 19:43:55 +0100

    Make sure that empty colormaps are white.

--- smoke/colormaps/abstractcolormap.cpp
+++ smoke/colormaps/abstractcolormap.cpp
@@ -1,10 +1,11 @@
 #include "abstractcolormap.h"
 
 #include <QDebug>
+#include <QColor>
 
 AbstractColorMap::AbstractColorMap(int numColors):
     numColors(numColors),
     image(new QImage(1, numColors, QImage::Format_ARGB32))
 {
-    qDebug() << "AbstractColorMap";
+    this->image->fill(QColor(Qt::white).rgb());
 }
