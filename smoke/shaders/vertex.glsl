32fc5a78e87b805219a733f600048c5d67d4ebe0
Unknown <bastiaanvanloon@gmail.com>
2017-02-17 13:04:02 +0100

    Scaling function for textureCoordinate

--- smoke/shaders/vertex.glsl
+++ smoke/shaders/vertex.glsl
@@ -10,8 +10,13 @@ layout(location = 0) out float vsTextureCoordinate;
 //Uniform in
 uniform mat4 mvpMatrix;
 
+float mapToUnitLength(float value, float oldMin, float oldMax)
+{
+    float slope = 1/ (oldMax - oldMin);
+    return slope * (value - oldMin);
+}
 void main(void)
 {
-    vsTextureCoordinate = inputTextureCoordinate;
+    vsTextureCoordinate = mapToUnitLength(inputTextureCoordinate,5,10);
     gl_Position = mvpMatrix * vec4(inputPosition, 1.0);
 }
