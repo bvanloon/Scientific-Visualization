987e715329fba80a4fedf6e093a324e62f0ef2dd
Laura Baakman <laurabaakman@gmail.com>
2017-02-19 18:12:19 +0100

    Removed canvas inner class.

--- smoke/settings/settings.cpp
+++ smoke/settings/settings.cpp
@@ -5,12 +5,10 @@
 
 Settings::Settings(QObject *parent) : QObject(parent)
 {
-    this->canvas = new Canvas();
     this->visualization = new Visualization();
 }
 
 Settings::~Settings()
 {
-    delete canvas;
     delete visualization;
 }
--- smoke/settings/settings.h
+++ smoke/settings/settings.h
@@ -12,19 +12,6 @@ public:
     explicit Settings(QObject *parent = 0);
     ~Settings();
 
-    class Canvas {
-
-        public:
-//            int width;
-//            int height;
-
-            Canvas()
-//                :
-//                width(-1),
-//                height(-1)
-            {}
-    };
-
     class Visualization {
         public:
             float vecScale;
@@ -34,7 +21,6 @@ public:
             {}
     };
 
-    Canvas *canvas;
     Visualization *visualization;
 };
 
