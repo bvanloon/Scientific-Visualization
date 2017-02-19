eda632fc98555a0632ff3bd2953e4810e729940d
Unknown <bastiaanvanloon@gmail.com>
2017-02-19 18:46:45 +0100

    Made colorMap flexible with the number of colors

--- smoke/colormaps/rainbowcolormap.cpp
+++ smoke/colormaps/rainbowcolormap.cpp
@@ -26,9 +26,9 @@ void RainbowColorMap::fill()
 QRgb RainbowColorMap::toRainbowColor(float f)
 {
     float g = (6 - 2 * dx) * f + dx;
-    int red = fmax(0, (3 - fabs(g - 4) - fabs(g - 5)) / 2.0) * 255;
-    int green = fmax(0, (4 - fabs(g - 2) - fabs(g - 4)) / 2.0) * 255;
-    int blue = fmax(0, (3 - fabs(g - 1) - fabs(g - 2)) / 2.0) * 255;
+    int red = fmax(0, (3 - fabs(g - 4) - fabs(g - 5)) / 2.0) * (numColors - 1);
+    int green = fmax(0, (4 - fabs(g - 2) - fabs(g - 4)) / 2.0) * (numColors - 1);
+    int blue = fmax(0, (3 - fabs(g - 1) - fabs(g - 2)) / 2.0) * (numColors - 1);
     return qRgb(red, green, blue);
 }
 
