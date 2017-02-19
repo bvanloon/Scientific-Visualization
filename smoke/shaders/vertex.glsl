f72e433bc4717b81ad278f4be9ad73ec5b45fd41
Unknown <bastiaanvanloon@gmail.com>
2017-02-19 18:30:59 +0100

    Added clamping possibility to vertex shader

--- smoke/shaders/vertex.glsl
+++ smoke/shaders/vertex.glsl
@@ -5,6 +5,13 @@ struct rangeInfo {
     float maximum;
 };
 
+struct clampInfo {
+    bool clampingOn;
+    float clampStart;
+    float clampEnd;
+};
+
+
 // Variable in
 layout(location = 0) in vec3 inputPosition;
 layout(location = 1) in float inputTextureCoordinate;
@@ -15,14 +22,31 @@ layout(location = 0) out float vsTextureCoordinate;
 //Uniform in
 uniform mat4 mvpMatrix;
 uniform rangeInfo range;
+uniform clampInfo clamping;
 
-float mapToUnitLength(float value, rangeInfo oldRange)
+float mapToUnitLength(float value, rangeInfo oldRange, clampInfo clampValues)
 {
-    float slope = 1/ (oldRange.maximum - oldRange.minimum);
-    return slope * (value - oldRange.minimum);
+    float slopeMin = oldRange.minimum;
+    float slopeMax = oldRange.maximum;
+
+    if (clampValues.clampingOn)
+    {
+        slopeMin = oldRange.minimum * clampValues.clampStart + oldRange.minimum;
+        slopeMax = oldRange.maximum * clampValues.clampEnd;
+
+        if (value < slopeMin)
+            value = slopeMin;
+
+        if (value > slopeMax)
+            value = slopeMax;
+    }
+
+    float slope = 1/ (slopeMax - slopeMin);
+    return slope * (value - slopeMin);
 }
+
 void main(void)
 {
-    vsTextureCoordinate = mapToUnitLength(inputTextureCoordinate,range);
+    vsTextureCoordinate = mapToUnitLength(inputTextureCoordinate, range, clamping);
     gl_Position = mvpMatrix * vec4(inputPosition, 1.0);
 }
