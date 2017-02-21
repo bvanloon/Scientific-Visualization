1b18ec1f66404c130c93ee00c1ae5d9a646c5b02
Laura Baakman <laurabaakman@gmail.com>
2017-02-21 14:11:28 +0100

    Initialize the sliders to the default value.

--- smoke/view/visualizationsettingpane.cpp
+++ smoke/view/visualizationsettingpane.cpp
@@ -1,6 +1,7 @@
 #include "visualizationsettingpane.h"
 #include "ui_visualizationsettingspane.h"
 #include "settings/settings.h"
+#include "utilities.h"
 #include <QDebug>
 
 VisualizationSettingPane::VisualizationSettingPane(QWidget *parent) :
@@ -25,4 +26,18 @@ void VisualizationSettingPane::on_clampingCheckBox_clicked(bool checked)
 void VisualizationSettingPane::setUItoDefaults()
 {
     this->ui->clampingCheckBox->setChecked(Settings::defaults::visualization::clampingOn);
+    this->ui->clampingMinimumSlider->setValue(
+                mapToRange(
+                    Settings::defaults::visualization::clampStart,
+                    0.0f,
+                    1.0f,
+                    (float) this->ui->clampingMinimumSlider->minimum(),
+                    (float) this->ui->clampingMinimumSlider->maximum()));
+    this->ui->clampingMaximumSlider->setValue(
+                mapToRange(
+                    Settings::defaults::visualization::clampEnd,
+                    0.0f,
+                    1.0f,
+                    (float) this->ui->clampingMaximumSlider->minimum(),
+                    (float) this->ui->clampingMaximumSlider->maximum()));
 }
