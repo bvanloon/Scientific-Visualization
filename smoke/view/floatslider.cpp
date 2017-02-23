ce6ac0b1b6c5e1b47f38490d09b7cb26ab7702f4
Laura Baakman <laurabaakman@gmail.com>
2017-02-23 21:00:16 +0100

    Set the values of the different elements in the float slider.

--- smoke/view/floatslider.cpp
+++ smoke/view/floatslider.cpp
@@ -1,6 +1,9 @@
 #include "floatslider.h"
 #include "ui_floatslider.h"
 
+#include <QString>
+#include "utilities.h"
+
 FloatSlider::FloatSlider(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::FloatSlider)
@@ -11,4 +14,44 @@ FloatSlider::FloatSlider(QWidget *parent) :
 FloatSlider::~FloatSlider()
 {
     delete ui;
+}
+
+void FloatSlider::init(float minimum, float maximum, float value)
+{
+    setMinimum(minimum);
+    setMaximum(maximum);
+    setValue(value);
+}
+
+void FloatSlider::setMinimum(float minimum)
+{
+    this->minimum = minimum;
+    this->setLabel(this->ui->minimumLabel, minimum);
+}
+
+void FloatSlider::setMaximum(float maximum)
+{
+    this->maximum= maximum;
+    this->setLabel(this->ui->maximumLabel, maximum);
+}
+
+void FloatSlider::setRange(float minimum, float maximum)
+{
+    this->setMinimum(minimum);
+    this->setMaximum(maximum);
+}
+
+void FloatSlider::setValue(float value)
+{
+    this->setLabel(this->ui->valueLabel, value);
+    int position = qRound(
+                mapToRange(value,
+                           this->minimum, this->maximum,
+                           (float)this->ui->slider->minimum(), (float)this->ui->slider->maximum())
+                );
+    this->ui->slider->setValue(position);
+}
+
+void FloatSlider::setLabel(QLabel* label, float value){
+    label->setText(QString::number(value, 'f', 2));
 }
--- smoke/view/floatslider.h
+++ smoke/view/floatslider.h
@@ -2,6 +2,7 @@
 #define FLOATSLIDER_H
 
 #include <QWidget>
+#include <QLabel>
 
 namespace Ui {
 class FloatSlider;
@@ -15,8 +16,19 @@ public:
     explicit FloatSlider(QWidget *parent = 0);
     ~FloatSlider();
 
+    void init(float minimum, float maximum, float value);
+    void setMinimum(float minimum);
+    void setMaximum(float maximum);
+    void setRange(float minimum, float maximum);
+    void setValue(float value);
+
 private:
     Ui::FloatSlider *ui;
+
+    float minimum;
+    float maximum;
+
+    void setLabel(QLabel *label, float value);
 };
 
 #endif // FLOATSLIDER_H
