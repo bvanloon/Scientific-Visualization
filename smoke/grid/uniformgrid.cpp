7cfe23b50911ac4151802759ed9c91b845b9ee16
Laura Baakman <laurabaakman@gmail.com>
2017-02-26 15:08:22 +0100

    Store the vertex positions in vertexPositions

--- smoke/grid/uniformgrid.cpp
+++ smoke/grid/uniformgrid.cpp
@@ -1,10 +1,11 @@
 #include "uniformgrid.h"
 #include "settings/settings.h"
 #include "settings/simulationsettings.h"
+#include <QDebug>
 
 UniformGrid::UniformGrid(int dimension):
-    dimension(dimension),
-    Grid(dimension * dimension)
+    Grid(dimension * dimension),
+    dimension(dimension)
 {
     this->create();
 }
@@ -12,13 +13,23 @@ UniformGrid::UniformGrid(int dimension):
 void UniformGrid::create()
 {
     QVector3D position;
+    Vertex vertex;
+    int idx;
     for (int i = 0; i < dimension; i++){
         for (int j = 0; j < dimension; j++)
         {
+            idx = to1Dindex(i, j);
+            qDebug() << "(" << i <<", " << j << ") = " << idx;
             position = QVector3D(
                         Settings::simulation().cellSize.width() + (double)i * Settings::simulation().cellSize.width(),
                         Settings::simulation().cellSize.height() + (double)j * Settings::simulation().cellSize.height(),
                         0.0f);
+            this->vertexPositions.replace(idx, position);
         }
     }
+}
+
+int UniformGrid::to1Dindex(int i, int j)
+{
+    return (j * this->dimension) + i;
 }
