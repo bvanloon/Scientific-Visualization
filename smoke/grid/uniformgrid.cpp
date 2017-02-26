cc9ccbc55c34fba255a977ffabea1ca178094666
Laura Baakman <laurabaakman@gmail.com>
2017-02-26 14:21:29 +0100

    Very basic create that still uses the settings object and doesn't fill vertices or vertex positions.

--- smoke/grid/uniformgrid.cpp
+++ smoke/grid/uniformgrid.cpp
@@ -11,4 +11,14 @@ UniformGrid::UniformGrid(int dimension):
 
 void UniformGrid::create()
 {
+    QVector3D position;
+    for (int i = 0; i < dimension; i++){
+        for (int j = 0; j < dimension; j++)
+        {
+            position = QVector3D(
+                        Settings::simulation().cellSize.width() + (double)i * Settings::simulation().cellSize.width(),
+                        Settings::simulation().cellSize.height() + (double)j * Settings::simulation().cellSize.height(),
+                        0.0f);
+        }
+    }
 }
