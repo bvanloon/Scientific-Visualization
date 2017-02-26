abbc0acff451ba9fa4302247f3b2d3ef79d388dd
Laura Baakman <laurabaakman@gmail.com>
2017-02-26 15:33:16 +0100

    Stream operators overloaded.

--- smoke/grid/vertex.cpp
+++ smoke/grid/vertex.cpp
@@ -1,6 +1,25 @@
 #include "vertex.h"
 
-Vertex::Vertex(QVector3D *position)
-{
+Vertex::Vertex(const QVector3D *position):
+    position(position)
+{}
 
+const QVector3D *Vertex::getPosition() const
+{
+    return this->position;
+}
+
+QDebug operator<<(QDebug stream, const Vertex &vertex)
+{
+    stream << "Vertex ["
+           << "\tposition: "    << *vertex.position
+           << "]";
+    return stream;
+}
+
+
+QDebug operator<<(QDebug stream, Vertex *vertex)
+{
+    stream << *vertex;
+    return stream;
 }
