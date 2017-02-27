fa0d86b6b7ced9e0f89c5dc1a2b1a613ee715e77
Bastiaan <bastiaanvanloon@gmail.com>
2017-02-27 10:58:30 +0100

    Cold colormap implementation

--- smoke/colormaps/coldcolormap.h
+++ smoke/colormaps/coldcolormap.h
@@ -1,11 +1,18 @@
 #ifndef COLDCOLORMAP_H
 #define COLDCOLORMAP_H
 
+#include "colormaps/abstractcolormap.h"
 
 class Coldcolormap : public AbstractColorMap
 {
 public:
-    Coldcolormap();
+    static AbstractColorMap * Create(int numColors, float saturation);
+
+private:
+    Coldcolormap(int numColors, float saturation);
+
+    void fill();
+    QRgb toColdColor(float f);
 };
 
 #endif // COLDCOLORMAP_H
