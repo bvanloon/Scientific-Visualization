cd6f405e2ae7ebeec79f544171056a11adc1826d
Bastiaan <bastiaanvanloon@gmail.com>
2017-03-03 13:50:03 +0100

    Removed onSignalChanged functions from canvas

--- smoke/view/canvas.cpp
+++ smoke/view/canvas.cpp
@@ -17,16 +17,6 @@ Canvas::~Canvas()
     delete this->timer;
 }
 
-
-void Canvas::idleLoop()
-{
-    if(!Settings::simulation().frozen)
-    {
-        this->simulation->step();
-    }
-    update();
-}
-
 void Canvas::initiateIdleLoop()
 {
    this->timer->start();
@@ -39,9 +29,19 @@ void Canvas::initializeGL()
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
 
-    //this->vectorEngine = new VectorEngine();
+    this->vectorEngine = new VectorEngine();
     this->smokeEngine = new SmokeEngine();
     emit openGlReady();
+}
+
+
+void Canvas::idleLoop()
+{
+    if(!Settings::simulation().frozen)
+    {
+        this->simulation->step();
+    }
+    update();
 }
 
 void Canvas::setSimulation(Simulation *simulation)
@@ -60,8 +60,8 @@ void Canvas::mouseMoveEvent(QMouseEvent *event)
 void Canvas::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   
-    smokeEngine->draw(this->simulation);
-//    vectorEngine->draw(this->simulation);
+    //smokeEngine->draw(this->simulation);
+    vectorEngine->draw(this->simulation);
 
 }
 
@@ -79,53 +79,3 @@ void Canvas::resizeGL(int width, int height)
     //change to signal slot construction
     this->smokeEngine->AbstractEngine::setMVPMatrix();
 }
-
-
-//// ----- REMOVE  ------
-
-void Canvas::onValueRangeChanged(float minimum, float maximum)
-{
-    setColorMapValueRange(minimum, maximum);
-}
-
-void Canvas::onSetClamping(bool clampingOn)
-{
-    setColorMapClampingTo(clampingOn);
-}
-
-void Canvas::onsetClampingRange(float minimum, float maximum)
-{
-    setColorMapClampRange(minimum, maximum);
-}
-
-void Canvas::onColorMapChanged(AbstractColorMap colormap)
-{
-    if(isValid()){
-        this->smokeEngine->AbstractEngine::setTexture(colormap);
-    }
-
-}
-
-void Canvas::onForceChanged(float force)
-{
-    if(Settings::visualization().scalar == Settings::Visualization::ScalarVariable::fluidDensity){
-        setColorMapValueRange(0.0f, force);
-    }
-}
-
-void Canvas::setColorMapValueRange(float min, float max)
-{
-    this->smokeEngine->AbstractEngine::setColorMapValueRange(min,max);
-}
-
-void Canvas::setColorMapClampRange(float startClamp, float endClamp)
-{
-    this->smokeEngine->AbstractEngine::setColorMapClampRange(startClamp, endClamp);
-}
-
-void Canvas::setColorMapClampingTo(bool clampingOn)
-{
-    this->smokeEngine->AbstractEngine::setColorMapClampingTo(clampingOn);
-}
-
-
