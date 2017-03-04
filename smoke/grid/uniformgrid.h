d8e7e589f603e99ed74620006b80743efb29cf58
Laura Baakman <laurabaakman@gmail.com>
2017-03-04 14:35:52 +0100

    Made getPadding public.

--- smoke/grid/uniformgrid.h
+++ smoke/grid/uniformgrid.h
@@ -17,8 +17,8 @@ public:
     static UniformGrid *createSimulationGrid(int dimension, QSizeF size, SimulationRealization *simulation);
     static UniformGrid *createVisualizationGrid(int dimension, QSizeF size, UniformGrid* simulationGrid);
 
+    QSizeF const& getPadding() const;
     QSizeF getCellSize() const;
-
     int getDimension() const;
 
 private:
@@ -39,8 +39,6 @@ private:
     Cell* findCellContaining(QVector3D position);
 
     QVector3D computeVertexPosition(int i, int j);
-
-    QSizeF const& getPadding() const;
 
     Vertex* getVertexAt(int x, int y) const;
 
