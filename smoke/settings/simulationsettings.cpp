cb08a7fcdb987c4d993519d9cf1282d3c2f82e6c
Laura Baakman <laurabaakman@gmail.com>
2017-03-06 18:46:56 +0100

    Formatting and output a reminder.

--- smoke/settings/simulationsettings.cpp
+++ smoke/settings/simulationsettings.cpp
@@ -3,61 +3,73 @@
 #include "fftw.h"
 #include <QDebug>
 
-Settings::Simulation::Simulation(QObject *parent):
-    QObject(parent),
-    dimension(50),
-    frozen(false),
-    force(10.0f),
-    timestep(0.4),
-    cellSize(-1, -1)
-{}
-
-const Settings::Simulation &Settings::Simulation::instance()
+Settings::Simulation::Simulation(QObject *parent) :
+   QObject(parent),
+   dimension(50),
+   frozen(false),
+   force(10.0f),
+   timestep(0.4),
+   cellSize(-1, -1)
 {
-    static Simulation instance;
-    return instance;
 }
+
+
+const Settings::Simulation& Settings::Simulation::instance()
+{
+   static Simulation instance;
+
+   return instance;
+}
+
 
 void Settings::Simulation::onDimensionChanged(int newDimension)
 {
-    this->dimension = newDimension;
-    updateGridCellSize();
+   this->dimension = newDimension;
+   updateGridCellSize();
 }
+
 
 void Settings::Simulation::onForceChanged(float newForce)
 {
-    this->force = newForce;
-    emit forceChanged(newForce);
+   this->force = newForce;
+   emit forceChanged(newForce);
 }
+
 
 void Settings::Simulation::onWindowResized(int width, int height)
 {
-    updateGridCellSize(width, height);
+   updateGridCellSize(width, height);
+   qDebug() << "Settings::Simulation::onWindowResized: Notify the visualization grid of the changed cell size";
 }
+
 
 void Settings::Simulation::onToggleFrozen()
 {
-    this->frozen = !this->frozen;
+   this->frozen = !this->frozen;
 }
+
 
 void Settings::Simulation::onToggleFrozenKeyboard()
 {
-    this->frozen = !this->frozen;
-    emit toggleFrozen(this->frozen);
+   this->frozen = !this->frozen;
+   emit toggleFrozen(this->frozen);
 }
+
 
 void Settings::Simulation::onTimeStepChanged(float value)
 {
-    this->timestep = value;
+   this->timestep = value;
 }
+
 
 void Settings::Simulation::updateGridCellSize()
 {
-    updateGridCellSize(Settings::canvas().size.width(), Settings::canvas().size.height());
+   updateGridCellSize(Settings::canvas().size.width(), Settings::canvas().size.height());
 }
+
 
 void Settings::Simulation::updateGridCellSize(int width, int height)
 {
-    cellSize.setHeight((fftw_real) height / (fftw_real)(dimension + 1));
-    cellSize.setWidth((fftw_real) width / (fftw_real)(dimension + 1));
+   cellSize.setHeight((fftw_real)height / (fftw_real)(dimension + 1));
+   cellSize.setWidth((fftw_real)width / (fftw_real)(dimension + 1));
 }
