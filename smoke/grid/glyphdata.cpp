6dc413cbba207208c5b054747817eb387b88e8dd
Bastiaan <bastiaanvanloon@gmail.com>
2017-03-10 16:26:54 +0100

    Fixed

--- smoke/grid/glyphdata.cpp
+++ smoke/grid/glyphdata.cpp
@@ -29,9 +29,9 @@ void GlyphData::extend(GlyphData other)
 
 void GlyphData::addGlyph(QVector3D position, QVector2D direction, float textureCoordinate)
 {
-   positions.append(position);
-   direction.append(direction);
-   direction.append(textureCoordinate);
+   this->positions.append(position);
+   this->directions.append(direction);
+   this->textureCoordinates.append(textureCoordinate);
 }
 
 QVector<QVector3D> GlyphData::getPositions() const
