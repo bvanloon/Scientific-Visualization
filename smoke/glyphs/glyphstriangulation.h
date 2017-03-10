be0816a5ad077dc893621c69bf0fee81867a3755
Bastiaan <bastiaanvanloon@gmail.com>
2017-03-10 15:05:57 +0100

    Implemented Triangulation

--- smoke/glyphs/glyphstriangulation.h
+++ smoke/glyphs/glyphstriangulation.h
@@ -1,11 +1,24 @@
 #ifndef GLYPHSTRIANGULATION_H
 #define GLYPHSTRIANGULATION_H
 
+#include <QVector>
+#include <QVector3D>
 
+#include "glyphs/abstractglyph.h"
 class GlyphsTriangulation
 {
-public:
-    GlyphsTriangulation();
+   public:
+      GlyphsTriangulation();
+
+      void extend(AbstractGlyph glyph);
+
+      QVector<QVector3D> getVertices() const;
+
+      QVector<float> getTextureCoordinates() const;
+
+   private:
+      QVector<QVector3D> vertices;
+      QVector<float> textureCoordinates;
 };
 
 #endif // GLYPHSTRIANGULATION_H
