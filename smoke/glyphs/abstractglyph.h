66b091df03f9575f1ac81bf9e0385fbae2891818
Laura Baakman <laurabaakman@gmail.com>
2017-03-13 10:40:52 +0100

    Formatting.

--- smoke/glyphs/abstractglyph.h
+++ smoke/glyphs/abstractglyph.h
@@ -18,14 +18,13 @@ class AbstractGlyph
 
       QVector<QVector3D> getNormals() const;
 
-protected:
+   protected:
       float textureCoordinate;
       QVector<QVector3D> normals;
       QVector<QVector3D> vertices;
       void addVertex(QVector3D vertex, QVector3D normal);
 
-    private:
-
+   private:
 };
 
 #endif // ABSTRACTGLYPH_H
