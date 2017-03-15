708ef9910ddcddb7dd19022a4dc09f3cd930aa61
Laura Baakman <laurabaakman@gmail.com>
2017-03-15 12:13:38 +0100

    Add clas to handle the building of an airplane.

--- smoke/glyphs/airplane.cpp
+++ smoke/glyphs/airplane.cpp
@@ -1,9 +1,30 @@
 #include "airplane.h"
+#include "utilities/mesh.h"
 
 Airplane::Airplane(QVector3D position, QVector3D direction, float scalar) :
    AbstractGlyph(scalar)
 {
-   addVertex(QVector3D(10.00, 20.0, 0.0), QVector3D(0.0, 0.0, 1.0));
-   addVertex(QVector3D(200.0, 100.0, 0.0), QVector3D(0.0, 0.0, 1.0));
-   addVertex(QVector3D(100.0, 150.0, 0.0), QVector3D(0.0, 0.0, 1.0));
+    AirplaneBuilder boeing = AirplaneBuilder(position, direction);
+    addVertices(boeing.getVertices(), boeing.getNormals());
+}
+
+Airplane::AirplaneBuilder::AirplaneBuilder(QVector3D position, QVector3D direction) :
+   position(position),
+   direction(direction)
+{}
+
+QVector<QVector3D> Airplane::AirplaneBuilder::getNormals()
+{
+   QVector<QVector3D> normals;
+   normals.fill(QVector3D(0.0, 0.0, 1.0), 3);
+   return normals;
+}
+
+QVector<QVector3D> Airplane::AirplaneBuilder::getVertices()
+{
+    QVector<QVector3D> vertices;
+    vertices.append(QVector3D(10.00, 20.0, 0.0));
+    vertices.append(QVector3D(200.0, 100.0, 0.0));
+    vertices.append(QVector3D(100.0, 150.0, 0.0));
+    return vertices;
 }
--- smoke/glyphs/airplane.h
+++ smoke/glyphs/airplane.h
@@ -6,8 +6,23 @@
 
 class Airplane : public AbstractGlyph
 {
-public:
-    Airplane(QVector3D position, QVector3D direction, float scalar);
+   public:
+      Airplane(QVector3D position, QVector3D direction, float scalar);
+
+   private:
+      class AirplaneBuilder;
+};
+
+class Airplane::AirplaneBuilder {
+   public:
+      AirplaneBuilder(QVector3D position, QVector3D direction);
+
+      QVector<QVector3D> getNormals();
+      QVector<QVector3D> getVertices();
+
+   private:
+      QVector3D position;
+      QVector3D direction;
 };
 
 #endif // AIRPLANE_H
