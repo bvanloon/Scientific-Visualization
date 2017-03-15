cbfe03434b550aefe73e1214659a6d4a0783e6f2
Laura Baakman <laurabaakman@gmail.com>
2017-03-15 16:32:24 +0100

    Compute equilateral triangles.

--- smoke/glyphs/airplane.cpp
+++ smoke/glyphs/airplane.cpp
@@ -4,47 +4,51 @@
 
 #define SIN60    0.86602540378
 
-const float Airplane::AirplaneBuilder::minSize = 10;
-const float Airplane::AirplaneBuilder::maxSize = 100;
+const float Airplane::AirplaneBuilder::minSize = 1;
+const float Airplane::AirplaneBuilder::maxSize = 20;
 
 Airplane::Airplane(QVector3D position, QVector3D direction, float scalar) :
    AbstractGlyph(scalar)
 {
-   AirplaneBuilder boeing = AirplaneBuilder(QVector3D(20.0, 150.0, 0.0), QVector3D(1.0, 0.0, 0.0));
-//   AirplaneBuilder boeing = AirplaneBuilder(position, direction);
+   AirplaneBuilder builder = AirplaneBuilder(position, direction);
 
-   addVertices(boeing.getVertices(), boeing.getNormals());
+   addVertices(builder.getVertices(), builder.getNormals());
 }
 
 Airplane::AirplaneBuilder::AirplaneBuilder(QVector3D position, QVector3D direction) :
-   direction(direction)
+   direction(direction.normalized()),
+   mesh(4, 2)
 {
-   float height = (maxSize + minSize) / SIN60;
+   float edgeLength = maxSize + minSize;
+   float height = edgeLength * SIN60;
 
-   QVector3D orthogonalDirection = computeOrthogonalVector(direction);
+   QVector3D orthogonalDirection = computeOrthogonalVector(this->direction);
 
    mesh::Vertex *d = mesh.addVertex(position);
+   mesh::Vertex *b = mesh.addVertex(d->position() + height * this->direction);
+   mesh::Vertex *c = mesh.addVertex(d->position() + 0.5 * edgeLength * orthogonalDirection);
+   mesh::Vertex *a = mesh.addVertex(d->position() - 0.5 * edgeLength * orthogonalDirection);
 
-   mesh::Vertex *b = mesh.addVertex(d->position() + height * direction);
-
-   mesh::Vertex *c = mesh.addVertex(d->position() + 0.5 * height * orthogonalDirection);
-   mesh::Vertex *a = mesh.addVertex(d->position() - 0.5 * height * orthogonalDirection);
+   mesh.addTriangle(a, b, d);
+   mesh.addTriangle(d, b, c);
 }
 
 QVector<QVector3D> Airplane::AirplaneBuilder::getNormals()
 {
-    QVector<QVector3D> normals;
-    normals.append(QVector3D(0.0, 0.0, 1.0));
-    normals.append(QVector3D(0.0, 0.0, 1.0));
-    normals.append(QVector3D(0.0, 0.0, 1.0));
-    return normals;
+   QVector<QVector3D> normals;
+   for (auto triangle : mesh.getTriangles())
+   {
+      normals.append(triangle->getVertexNormals());
+   }
+   return normals;
 }
 
 QVector<QVector3D> Airplane::AirplaneBuilder::getVertices()
 {
-    QVector<QVector3D> vertices;
-    vertices.append(QVector3D(10.0, 300.0, 0.0));
-    vertices.append(QVector3D(220.0, 250.0, 0.0));
-    vertices.append(QVector3D(100.0, 0.0, 0.0));
-    return vertices;
+   QVector<QVector3D> vertices;
+   for (auto triangle : mesh.getTriangles())
+   {
+      vertices.append(triangle->getVertexPositions());
+   }
+   return vertices;
 }
