7f99a88249c1b4ae0819fbd939f846f5689f740c
Laura Baakman <laurabaakman@gmail.com>
2017-03-15 22:19:29 +0100

    Make triangle glyphs adaptive to the cellsize.

--- smoke/glyphs/triangle.h
+++ smoke/glyphs/triangle.h
@@ -8,6 +8,9 @@ class Triangle : public AbstractGlyph
 {
    public:
       Triangle(QVector3D position, QVector3D direction, float scalar);
+
+private:
+      static const double cellRatio;
 };
 
 #endif // TRIANGLE_H
