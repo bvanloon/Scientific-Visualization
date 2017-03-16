5d834245d17ce563bfab2d17c30a9279a2c21035
Laura Baakman <laurabaakman@gmail.com>
2017-03-16 13:36:44 +0100

    Completed the computation of the mesh of the cone.

--- smoke/shapes/cone.cpp
+++ smoke/shapes/cone.cpp
@@ -1,10 +1,14 @@
 #include "cone.h"
 #include <math.h>
+#include "utilities/vector.h"
+#include <QMatrix4x4>
+#include <QVector4D>
 
 Cone::Cone() :
    center(QVector3D(0, 0, 0)),
    direction(QVector3D(0, 1, 0)),
-   height(1.0f)
+   height(1.0f),
+   radius(1.0f)
 {}
 
 mesh::TriangleMesh *Cone::toTriangleMesh(int resolution)
@@ -32,15 +36,59 @@ Cone::MeshBuilder::MeshBuilder(Cone *cone, int resolution) :
    cone(cone),
    resolution(resolution)
 {
-   mesh = new mesh::TriangleMesh(computeNumVertices(), computeNumFaces());
+   this->perpendicularDirection = QVector3D(0, 0, 1);
+   this->mesh = new mesh::TriangleMesh(computeNumVertices(), computeNumFaces());
+   this->top = this->mesh->addVertex(computeTopPosition());
+   this->bottomCenter = this->mesh->addVertex(computeBottomCenterPosition());
+   this->addFaces();
 }
 
-QVector3D Cone::MeshBuilder::computeTop()
+QVector3D Cone::MeshBuilder::computeTopPosition()
 {
-   return this->cone->center + 0.5 * this->cone->height * this->cone->direction;
+   return this->cone->center + this->cone->height * this->cone->direction;
 }
 
-QVector3D Cone::MeshBuilder::computeBottomCenter()
+QVector3D Cone::MeshBuilder::computeBottomCenterPosition()
 {
-   return this->cone->center - 0.5 * this->cone->height * this->cone->direction;
+   return this->cone->center;
+}
+
+void Cone::MeshBuilder::addFaces()
+{
+   QMatrix4x4 transformationMatrix;
+   transformationMatrix.rotate(360.0 / this->resolution, this->cone->direction);
+
+   QVector4D nextPosition = QVector4D(bottomCenter->position(), 1.0) + cone->radius * perpendicularDirection;
+   QVector4D currentPosition;
+
+   mesh::Vertex *firstVertex, *nextVertex, *currentVertex;
+   firstVertex = nextVertex = mesh->addVertex(nextPosition.toVector3D());
+
+   for (int i = 1; i < this->resolution; i++)
+   {
+      currentPosition = nextPosition;
+      currentVertex = nextVertex;
+
+      nextPosition = currentPosition * transformationMatrix;
+      nextVertex = mesh->addVertex(nextPosition.toVector3D());
+
+      addFacesWithEdge(currentVertex, nextVertex);
+   }
+   addFacesWithEdge(nextVertex, firstVertex);
+}
+
+void Cone::MeshBuilder::addFacesWithEdge(mesh::Vertex *circleVertexA, mesh::Vertex *circleVertexB)
+{
+    addBottomFace(circleVertexA, circleVertexB);
+    addSideFace(circleVertexA, circleVertexB);
+}
+
+void Cone::MeshBuilder::addBottomFace(mesh::Vertex *circleVertexA, mesh::Vertex *circleVertexB)
+{
+    this->mesh->addTriangle(circleVertexA, circleVertexB, this->bottomCenter);
+}
+
+void Cone::MeshBuilder::addSideFace(mesh::Vertex *circleVertexA, mesh::Vertex *circleVertexB)
+{
+    this->mesh->addTriangle(circleVertexA, circleVertexB, this->top);
 }
