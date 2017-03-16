b8edfc05c53b652a8cdfb868624c89e8c5bb22b1
Laura Baakman <laurabaakman@gmail.com>
2017-03-16 17:17:24 +0100

    Computed the rotation matrix.

--- smoke/glyphs/cone.cpp
+++ smoke/glyphs/cone.cpp
@@ -3,15 +3,16 @@
 #include "shapes/cone.h"
 #include <QMatrix4x4>
 #include <QDebug>
+#include <math.h>
 
-const double Cone::maxCellRatio = 1.0;
+const double Cone::maxCellRatio = 2.0;
 
 Cone::Cone(QVector3D position, QVector3D direction, float scalar) :
    AbstractGlyph(scalar),
    mesh(::shapes::Cone().toTriangleMesh())
 {
-    transform(position, direction);
-    this->addVertices(this->mesh->getVerticesAsVBO(), this->mesh->getNormalsAsVBO());
+   transform(position, direction);
+   this->addVertices(this->mesh->getVerticesAsVBO(), this->mesh->getNormalsAsVBO());
 }
 
 void Cone::transform(QVector3D position, QVector3D direction)
@@ -23,15 +24,15 @@ void Cone::transform(QVector3D position, QVector3D direction)
 
 void Cone::translate(QVector3D position)
 {
-    QMatrix4x4 translationMatrix;
-    translationMatrix.translate(position);
-    mesh->applyTransformation(translationMatrix);
+   QMatrix4x4 translationMatrix;
+
+   translationMatrix.translate(position);
+   mesh->applyTransformation(translationMatrix);
 }
 
 void Cone::scale(QVector3D direction)
 {
-//    float scalingFactor = computeScalingFactor(direction);
-   float scalingFactor = 10;
+   float scalingFactor = computeScalingFactor(direction);
 
    QMatrix4x4 scalingMatrix;
 
@@ -40,11 +41,21 @@ void Cone::scale(QVector3D direction)
 }
 
 void Cone::rotate(QVector3D direction)
-{}
+{
+   //http://stackoverflow.com/questions/20177506/rotate-geometry-to-align-to-a-direction-vector
+   //http://tonyobryan.com/index.php?article=28
+   QVector3D rotationAxis = QVector3D::crossProduct(shapes::Cone::getDefaultDirection(), direction.normalized());
+   float rotationAngle = acos(QVector3D::dotProduct(rotationAxis.normalized(), direction.normalized()));
+
+   QMatrix4x4 rotationMatrix;
+
+   rotationMatrix.rotate(rotationAngle, rotationAxis);
+   mesh->applyTransformation(rotationMatrix);
+}
 
 float Cone::computeScalingFactor(QVector3D direction)
 {
-   return computeBaseSize(maxCellRatio) *
-          computeNormalizedMagnitude(direction) *
-          Settings::visualization::glyphs().scale;
+   return computeBaseSize(maxCellRatio)
+          * computeNormalizedMagnitude(direction)
+          * Settings::visualization::glyphs().scale;
 }
