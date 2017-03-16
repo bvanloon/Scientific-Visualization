154f267eb8f2bc638f7558062f536300d01d3e1c
Laura Baakman <laurabaakman@gmail.com>
2017-03-16 13:54:48 +0100

    Set up the glyph class.

--- smoke/glyphs/cone.h
+++ smoke/glyphs/cone.h
@@ -2,11 +2,25 @@
 #define CONE_H
 
 #include "glyphs/abstractglyph.h"
+#include "utilities/mesh.h"
 
 class Cone : public AbstractGlyph
 {
 public:
     Cone(QVector3D position, QVector3D direction, float scalar);
+
+    static const mesh::TriangleMesh* unitCone;
+
+private:
+    mesh::TriangleMesh* cone;
+
+    static const double maxCellRatio;
+
+    float computeScalingFactor(float magnitude);
+
+    void translate(QVector3D position);
+    void scale(float scalingFactor);
+    void rotate(QVector3D direction);
 };
 
 #endif // CONE_H
