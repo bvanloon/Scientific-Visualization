f74ecabd1070a42a3ceef9227ec81e3b8a716b8f
Laura Baakman <laurabaakman@gmail.com>
2017-03-17 11:39:37 +0100

    Header with macro to avoid unused warnings.

---
+++ smoke/unused.h
@@ -0,0 +1,17 @@
+#ifndef UNUSED_H
+#define UNUSED_H
+
+#ifdef __GNUC__
+#  define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
+#else
+#  define UNUSED(x) UNUSED_ ## x
+#endif
+
+#ifdef __GNUC__
+#  define UNUSED_FUNCTION(x) __attribute__((__unused__)) UNUSED_ ## x
+#else
+#  define UNUSED_FUNCTION(x) UNUSED_ ## x
+#endif
+
+#endif // UNUSED_H
+
