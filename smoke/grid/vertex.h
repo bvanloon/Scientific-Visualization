f4999dff0509b307232ab92a3fd55ce20f8beba2
Laura Baakman <laurabaakman@gmail.com>
2017-03-21 09:44:51 +0100

    Formatting.

--- smoke/grid/vertex.h
+++ smoke/grid/vertex.h
@@ -10,96 +10,109 @@ class Cell;
 
 class Vertex
 {
-public:
-    Vertex(const QVector3D *position = NULL);
-    virtual ~Vertex();
+   public:
+      Vertex(const QVector3D *position = NULL);
+      virtual ~Vertex();
 
-    typedef float (Vertex::*scalarGetter)() const;
-    typedef QVector2D (Vertex::*vectorGetter)() const;
+      typedef float (Vertex::*scalarGetter)() const;
+      typedef QVector2D (Vertex::*vectorGetter)() const;
 
-    const QVector3D* getPosition() const;
+      const QVector3D *getPosition() const;
 
-    friend QDebug operator<<(QDebug stream, const Vertex &vertex);
-    friend QDebug operator<<(QDebug stream, Vertex *vertex);
+      friend QDebug operator<<(QDebug stream, const Vertex& vertex);
 
-    virtual QVector2D getFluidVelocity() const = 0;
-    virtual float getFluidVelocityMagnitude() const = 0;
+      friend QDebug operator<<(QDebug stream, Vertex *vertex);
 
-    virtual QVector2D getForce() const = 0;
-    virtual float getForceMagnitude() const = 0;
+      virtual QVector2D getFluidVelocity() const = 0;
 
-    virtual float getFluidDensity() const = 0;
+      virtual float getFluidVelocityMagnitude() const = 0;
 
-    static Vertex::scalarGetter getScalarGetter(Settings::sim::Scalar scalar);
-    static Vertex::vectorGetter getVectorGetter(Settings::sim::Vector vector);
+      virtual QVector2D getForce() const = 0;
 
-protected:
-    const QVector3D *position;
+      virtual float getForceMagnitude() const = 0;
 
+      virtual float getFluidDensity() const = 0;
+
+      static Vertex::scalarGetter getScalarGetter(Settings::sim::Scalar scalar);
+
+      static Vertex::vectorGetter getVectorGetter(Settings::sim::Vector vector);
+
+   protected:
+      const QVector3D *position;
 };
 
-class StructuredGridVertex: public Vertex{
-public:
-    virtual QVector2D getFluidVelocity() const = 0;
-    virtual float getFluidVelocityMagnitude() const = 0;
+class StructuredGridVertex : public Vertex {
+   public:
+      virtual QVector2D getFluidVelocity() const = 0;
 
-    virtual QVector2D getForce() const = 0;
-    virtual float getForceMagnitude() const = 0;
+      virtual float getFluidVelocityMagnitude() const = 0;
 
-    virtual float getFluidDensity() const = 0;
+      virtual QVector2D getForce() const = 0;
 
-    Cell *getLowerRightCell() const;
-    void setLowerRightCell(Cell *value);
-    bool hasLowerRightCell();
+      virtual float getForceMagnitude() const = 0;
 
-protected:
-    StructuredGridVertex(const QVector3D *position);
+      virtual float getFluidDensity() const = 0;
 
-    Cell *lowerRightCell;
+      Cell *getLowerRightCell() const;
+
+      void setLowerRightCell(Cell *value);
+
+      bool hasLowerRightCell();
+
+   protected:
+      StructuredGridVertex(const QVector3D *position);
+
+      Cell *lowerRightCell;
 };
 
-class SimulationVertex: public StructuredGridVertex
+class SimulationVertex : public StructuredGridVertex
 {
-public:
+   public:
 
-    SimulationVertex(const QVector3D *position, double *vx, double *vy, double *fx, double *fy, double *rho);
+      SimulationVertex(const QVector3D *position, double *vx, double *vy, double *fx, double *fy, double *rho);
 
-    friend QDebug operator<<(QDebug stream, const SimulationVertex &vertex);
-    friend QDebug operator<<(QDebug stream, SimulationVertex *vertex);
+      friend QDebug operator<<(QDebug stream, const SimulationVertex& vertex);
 
-    virtual QVector2D getFluidVelocity() const;
-    virtual float getFluidVelocityMagnitude() const;
+      friend QDebug operator<<(QDebug stream, SimulationVertex *vertex);
 
-    virtual QVector2D getForce() const;
-    virtual float getForceMagnitude() const;
+      virtual QVector2D getFluidVelocity() const;
 
-    virtual float getFluidDensity() const;
+      virtual float getFluidVelocityMagnitude() const;
 
-private:
-    double *vx, *vy;
-    double *fx, *fy;
-    double *rho;
+      virtual QVector2D getForce() const;
 
+      virtual float getForceMagnitude() const;
+
+      virtual float getFluidDensity() const;
+
+   private:
+      double *vx, *vy;
+      double *fx, *fy;
+      double *rho;
 };
 
-class VisualizationVertex: public StructuredGridVertex
+class VisualizationVertex : public StructuredGridVertex
 {
-public:
-    VisualizationVertex(const QVector3D *position, Cell* containingCell);
+   public:
+      VisualizationVertex(const QVector3D *position, Cell *containingCell);
 
-    friend QDebug operator<<(QDebug stream, const VisualizationVertex &vertex);
-    friend QDebug operator<<(QDebug stream, VisualizationVertex *vertex);
+      friend QDebug operator<<(QDebug stream, const VisualizationVertex& vertex);
 
-    virtual QVector2D getFluidVelocity() const;
-    virtual float getFluidVelocityMagnitude() const;
+      friend QDebug operator<<(QDebug stream, VisualizationVertex *vertex);
 
-    virtual QVector2D getForce() const;
-    virtual float getForceMagnitude() const;
+      virtual QVector2D getFluidVelocity() const;
 
-    virtual float getFluidDensity() const;
+      virtual float getFluidVelocityMagnitude() const;
 
-private:
-    Cell* containingCell;
+      virtual QVector2D getForce() const;
+
+      virtual float getForceMagnitude() const;
+
+      virtual float getFluidDensity() const;
+
+
+   private:
+      Cell *containingCell;
 };
 
 
