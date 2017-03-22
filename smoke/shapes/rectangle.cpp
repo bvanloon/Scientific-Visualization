1db67a9674bca7eae1ac4bb26bea602834cda518
Laura Baakman <laurabaakman@gmail.com>
2017-03-22 14:22:05 +0100

    Build a rectangle and the associated mesh.

--- smoke/shapes/rectangle.cpp
+++ smoke/shapes/rectangle.cpp
@@ -1,7 +1,29 @@
 #include "rectangle.h"
 
-Rectangle::Rectangle()
+shapes::Rectangle::Rectangle(int width, int height) :
+   width(widht),
+   height(height)
+{}
+
+shapes::Rectangle::Rectangle(double size) :
+   width(size),
+   height(size)
+{}
+
+mesh::TriangleMesh *shapes::Rectangle::toTriangleMesh()
 {
+   mesh::TriangleMesh mesh;
 
+   double xAxisOffset = this->width / 2;
+   double yAxisOffset = this->height / 2;
+
+   mesh::Vertex *lowerLeft = mesh.addVertex(QVector3D(-xAxisOffset, -yAxisOffset, 0.0));
+   mesh::Vertex *lowerRight = mesh.addVertex(QVector3D(xAxisOffset, -yAxisOffset, 0.0));
+   mesh::Vertex *upperLeft = mesh.addVertex(QVector3D(-xAxisOffset, yAxisOffset, 0.0));
+   mesh::Vertex *upperRight = mesh.addVertex(QVector3D(xAxisOffset, yAxisOffset, 0.0));
+
+   mesh.addTriangle(lowerLeft, upperLeft, upperRight);
+   mesh.addTriangle(lowerLeft, upperRight, lowerRight);
+
+   return mesh;
 }
-
--- smoke/shapes/rectangle.h
+++ smoke/shapes/rectangle.h
@@ -1,11 +1,21 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
+#include "utilities/mesh.h"
 
-class Rectangle
-{
-public:
-    Rectangle();
-};
+namespace shapes {
+    class Rectangle
+    {
+       public:
+          Rectangle(double width = 1, double height = 1);
+          Rectangle(double size = 1);
+
+          mesh::TriangleMesh *toTriangleMesh();
+
+       private:
+          double width;
+          double height;
+    };
+}
 
 #endif // RECTANGLE_H
