2b20542278fad30e3956c3a0af01ce574397f50c
Laura Baakman <laurabaakman@gmail.com>
2017-03-22 11:42:11 +0100

    Clear the list of seedpoints.

--- smoke/settings/visualization/streamlines.cpp
+++ smoke/settings/visualization/streamlines.cpp
@@ -27,7 +27,7 @@ void Settings::visualization::StreamLines::onEdgeLengthFactorChanged(double newF
 
 void Settings::visualization::StreamLines::onClearSeedPoints()
 {
-    qDebug() << "Settings::visualization::StreamLines::onClearSeedPoints implement";
+    this->seedPoints.clear();
 }
 
 void Settings::visualization::StreamLines::onSeedPointAdded(QPointF newSeedPoint)
