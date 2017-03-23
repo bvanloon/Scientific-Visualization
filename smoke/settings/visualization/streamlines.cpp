99aaffad75390481b8d58920a99ccd995962f82b
Laura Baakman <laurabaakman@gmail.com>
2017-03-23 15:24:43 +0100

    Initialize the streamline settings object to the defaults.

--- smoke/settings/visualization/streamlines.cpp
+++ smoke/settings/visualization/streamlines.cpp
@@ -7,7 +7,9 @@ Settings::visualization::StreamLines::StreamLines(QObject *parent) :
    QObject(parent),
    colorMap(new ColorMap()),
    timeStep(1.0),
-   maximumTime(std::numeric_limits<double>::infinity())
+   maximumTime(std::numeric_limits<double>::infinity()),
+   edgeLengthFactor(Settings::defaults::visualization::streamlines::edgeLengthFactor),
+   totalLengthFactor(Settings::defaults::visualization::streamlines::totalLengthFactor)
 {
    colorMap->onTextureVariableChanged(Settings::sim::Scalar::fluidVelocityMagnitude);
    connectToOtherSettings();
