31e9edf5e4f90b6dc7a767b1bc80a4c3a0ea5536
Laura Baakman <laurabaakman@gmail.com>
2017-03-29 17:07:35 +0200

    Add vertices to the stateGrid.

--- smoke/grid/stategrid.cpp
+++ smoke/grid/stategrid.cpp
@@ -22,19 +22,25 @@ StateGrid::~StateGrid()
 void StateGrid::addVertices(UniformGrid *currentState)
 {
    QList<Index2D> vertexKeys = currentState->getVertexMap().keys();
-   Vertex *vertex;
+   Vertex *simulationVertex;
    Index2D key;
    for (int i = 0; i < vertexKeys.length(); i++)
    {
       key = vertexKeys[i];
-      vertex = currentState->getVertexMap().find(key).value();
-      addVertex(key, dynamic_cast<SimulationVertex *>(vertex));
+      simulationVertex = currentState->getVertexMap().find(key).value();
+      addVertex(key, dynamic_cast<SimulationVertex *>(simulationVertex));
    }
 }
 
 void StateGrid::addVertex(Index2D idx2D, SimulationVertex *vertex)
 {
-   qDebug() << index << ": " << vertex;
+   int idx1D = to1Dindex(idx2D);
+
+   StateVertex *stateVertex = new StateVertex(vertex);
+
+   this->vertexPositions.replace(idx1D, vertex->getPosition());
+   this->vertices.replace(idx1D, stateVertex);
+   this->vertexMap.insert(idx2D, stateVertex);
 }
 
 void StateGrid::addCells(UniformGrid *currentState)
