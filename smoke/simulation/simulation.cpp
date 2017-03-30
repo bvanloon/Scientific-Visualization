c6a66d2159e723dc4ac738e2967fe6ecd880da67
Laura Baakman <laurabaakman@gmail.com>
2017-03-30 15:01:21 +0200

    Formatting.

--- smoke/simulation/simulation.cpp
+++ smoke/simulation/simulation.cpp
@@ -23,24 +23,20 @@ Simulation::~Simulation()
    delete grid;
 }
 
-
 GlyphData Simulation::getGlyphData()
 {
    return getGlyphData(grid);
 }
-
 
 GlyphData Simulation::getGlyphData(Grid *grid)
 {
    return grid->getGlyphData();
 }
 
-
 Triangulation Simulation::getGridTriangulation()
 {
    return grid->getTriangulation();
 }
-
 
 QVector<float> Simulation::getTexCoord(Vertex::scalarGetter getter, Triangulation triangulation)
 {
@@ -55,12 +51,10 @@ QVector<float> Simulation::getTexCoord(Vertex::scalarGetter getter, Triangulatio
    return textureCoordinates;
 }
 
-
 QVector<float> Simulation::getTexCoordFluidDensity(Triangulation triangulation)
 {
    return getTexCoord(&Vertex::getFluidDensity, triangulation);
 }
-
 
 QVector<float> Simulation::getTexCoordFluidVelocityMagnitude(
    Triangulation triangulation)
@@ -68,19 +62,16 @@ QVector<float> Simulation::getTexCoordFluidVelocityMagnitude(
    return getTexCoord(&Vertex::getFluidVelocityMagnitude, triangulation);
 }
 
-
 QVector<float> Simulation::getTexCoordForceFieldMagnitude(
    Triangulation triangulation)
 {
    return getTexCoord(&Vertex::getForceMagnitude, triangulation);
 }
 
-
 void Simulation::step()
 {
    this->realization->do_one_simulation_step();
 }
-
 
 void Simulation::onMouseMoved(QPoint newPosition)
 {
@@ -91,18 +82,15 @@ void Simulation::onMouseMoved(QPoint newPosition)
    this->lastMousePosition = newPosition;
 }
 
-
 void Simulation::onStep()
 {
    this->step();
 }
 
-
 void Simulation::onWindowResized(int width, int height)
 {
    grid->changeGridArea(QSizeF(width, height));
 }
-
 
 UniformGrid *Simulation::getSimulationGrid() const
 {
