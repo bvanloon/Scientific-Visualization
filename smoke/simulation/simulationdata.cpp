14be500bc0bd378a5e47ffc6b425b003e0eaaeef
Laura Baakman <laurabaakman@gmail.com>
2017-03-30 12:07:00 +0200

    Fix the allocation.

--- smoke/simulation/simulationdata.cpp
+++ smoke/simulation/simulationdata.cpp
@@ -2,13 +2,9 @@
 
 SimulationData::SimulationData(int dimension)
 {
-   size_t mallocDimension = dimension * 2 * (dimension / 2 + 1) * sizeof(fftw_real);
-
-   vx = (fftw_real *)malloc(mallocDimension);
-   vy = (fftw_real *)malloc(mallocDimension);
-   fx = (fftw_real *)malloc(mallocDimension);
-   fy = (fftw_real *)malloc(mallocDimension);
-   rho = (fftw_real *)malloc(mallocDimension);
+   allocateVelocityData(dimension);
+   allocteForceData(dimension);
+   allocateDensityData(dimension);
 }
 
 SimulationData::~SimulationData()
@@ -18,4 +14,27 @@ SimulationData::~SimulationData()
    free(fx);
    free(fy);
    free(rho);
+}
+
+void SimulationData::allocateVelocityData(int dimension)
+{
+   size_t mallocDimension = dimension * 2 * (dimension / 2 + 1) * sizeof(fftw_real);
+
+   vx = (fftw_real *)malloc(mallocDimension);
+   vy = (fftw_real *)malloc(mallocDimension);
+}
+
+void SimulationData::allocteForceData(int dimension)
+{
+   size_t mallocDimension = dimension * dimension * sizeof(fftw_real);
+
+   fx = (fftw_real *)malloc(mallocDimension);
+   fy = (fftw_real *)malloc(mallocDimension);
+}
+
+void SimulationData::allocateDensityData(int dimension)
+{
+   size_t mallocDimension = dimension * dimension * sizeof(fftw_real);
+
+   rho = (fftw_real *)malloc(mallocDimension);
 }
--- smoke/simulation/simulationdata.h
+++ smoke/simulation/simulationdata.h
@@ -12,6 +12,11 @@ class SimulationData
       fftw_real *vx, *vy;         //(vx,vy)   = velocity field at the current moment
       fftw_real *fx, *fy;         //(fx,fy)   = user-controlled simulation forces, steered with the mouse
       fftw_real *rho;             //smoke density at the current (rho) and previous (rho0) moment
+
+private:
+      void allocateVelocityData(int dimension);
+      void allocteForceData(int dimension);
+      void allocateDensityData(int dimension);
 };
 
 #endif // SIMULATIONDATA_H
