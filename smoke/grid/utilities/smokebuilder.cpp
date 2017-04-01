865912def5a4c993e7b5bb6c881505cbc9381296
Laura Baakman <laurabaakman@gmail.com>
2017-04-01 17:55:27 +0200

    Set up a class to build the smoke object.

---
+++ smoke/grid/utilities/smokebuilder.cpp
@@ -0,0 +1,4 @@
+#include "smokebuilder.h"
+
+SmokeBuilder::SmokeBuilder()
+{}
---
+++ smoke/grid/utilities/smokebuilder.h
@@ -0,0 +1,11 @@
+#ifndef SMOKEBUILDER_H
+#define SMOKEBUILDER_H
+
+
+class SmokeBuilder
+{
+public:
+    SmokeBuilder();    
+};
+
+#endif // SMOKEBUILDER_H
