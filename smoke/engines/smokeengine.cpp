d187ebb3dd4daad351b71ba16cd4952da77d4c15
Laura Baakman <laurabaakman@gmail.com>
2017-04-02 12:18:30 +0200

    Use the smoke builder class.

--- smoke/engines/smokeengine.cpp
+++ smoke/engines/smokeengine.cpp
@@ -1,5 +1,5 @@
 #include "smokeengine.h"
-#include "grid/triangulation.h"
+#include "grid/utilities/smokebuilder.h"
 #include "settings/visualizationsettings.h"
 
 SmokeEngine::SmokeEngine() :
@@ -16,18 +16,9 @@ void SmokeEngine::draw(Simulation *simulation)
 
 int SmokeEngine::fillBuffers(Simulation *simulation)
 {
-   Triangulation triangulation = simulation->getGridTriangulation();
-
-   Vertex::scalarGetter textureGetter = Settings::visualization::smoke().colorMap->textureGetter;
-   GPUData data = GPUData(GL_TRIANGLES);
-   QVector3D normal = QVector3D(0.0, 0.0, 1.0);
-   double textureCoordinate;
-
-   for(Vertex* vertex : triangulation.getVertices()){
-        textureCoordinate = (vertex->*textureGetter)();
-        data.addElement(*vertex->getPosition(), normal, textureCoordinate);
-   }
+   SmokeBuilder builder = SmokeBuilder(simulation->getSimulationGrid(), colorMap->textureGetter);
+   GPUData data = builder.getGPUData();
 
    updateBuffers(data);
-   return triangulation.numVertices();
+   return data.numElements();
 }
