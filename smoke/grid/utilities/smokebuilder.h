aff0ff5b13dec50fc39740eb3b40c753298f04a7
Laura Baakman <laurabaakman@gmail.com>
2017-04-02 12:18:12 +0200

    Class to build the smoke.

--- smoke/grid/utilities/smokebuilder.h
+++ smoke/grid/utilities/smokebuilder.h
@@ -1,11 +1,25 @@
 #ifndef SMOKEBUILDER_H
 #define SMOKEBUILDER_H
 
+#include "utilities/gpudata.h"
+#include "grid/vertex.h"
+#include "grid/simulationgrid.h"
 
 class SmokeBuilder
 {
-public:
-    SmokeBuilder();    
+   public:
+      SmokeBuilder(SimulationGrid *grid, Vertex::scalarGetter textureGetter);
+
+      GPUData getGPUData() const;
+
+private:
+      SimulationGrid *grid;
+      Vertex::scalarGetter textureGetter;
+      GPUData data;
+      QVector3D normal;
+
+      void build();
+      void addVertex(Vertex *vertex);
 };
 
 #endif // SMOKEBUILDER_H
