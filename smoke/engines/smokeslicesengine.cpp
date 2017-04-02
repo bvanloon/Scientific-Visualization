3cb967358660fa50deaba29e9b2e0c302f6a4c8d
Laura Baakman <laurabaakman@gmail.com>
2017-04-02 16:49:43 +0200

    I am an idiot

--- smoke/engines/smokeslicesengine.cpp
+++ smoke/engines/smokeslicesengine.cpp
@@ -4,8 +4,7 @@
 
 SmokeSlicesEngine::SmokeSlicesEngine(SimulationGrid *grid) :
    AbstractSliceEngine(AbstractEngine::lightModel::noLight,
-                       Settings::engines::EnginesTypes::smokeSlices,
-                       computeToSliceTransformation()),
+                       Settings::engines::EnginesTypes::smokeSlices),
    simulation(grid)
 {
    connectToSettings();
@@ -15,6 +14,7 @@ void SmokeSlicesEngine::updateCache()
 {
    SmokeBuilder builder = SmokeBuilder(this->simulation, colorMap->textureGetter);
    GPUData newData = builder.getGPUData();
+   newData.transform(toSliceTransformation);
 
    this->cache.enqueue(newData);
 }
@@ -23,11 +23,4 @@ void SmokeSlicesEngine::connectToSettings()
 {
    connect(&Settings::visualization::smoke(), SIGNAL(clearCache()),
            this, SLOT(onClearCache()));
-}
-
-QMatrix4x4 SmokeSlicesEngine::computeToSliceTransformation()
-{
-    QMatrix4x4 transform;
-    qDebug() << "SmokeSlicesEngine::computeToSliceTransformation";
-    return transform;
 }
