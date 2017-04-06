c548dc774709f4925ab379c7e1a6edce074501fc
Laura Baakman <laurabaakman@gmail.com>
2017-04-06 16:49:01 +0200

    Removed the simulation and visualization grid from the glyphsliceengine.

--- smoke/engines/glyphsliceengine.cpp
+++ smoke/engines/glyphsliceengine.cpp
@@ -2,34 +2,28 @@
 #include "settings/settings.h"
 #include "settings/canvassettings.h"
 #include "visualizationbuilders/translucentglyphbuilder.h"
+#include "simulation/simulationstatehistory.h"
 
-GlyphSliceEngine::GlyphSliceEngine(SimulationGrid *simulationGrid) :
+GlyphSliceEngine::GlyphSliceEngine() :
    AbstractSliceEngine(AbstractEngine::lightModel::phongLight,
-                       Settings::engines::EnginesTypes::glyphSlices),
-   visualizationGrid(
-      JitterGrid::createVisualizationGrid(
-         Settings::defaults::visualization::glyphs::gridSize.width(),
-         Settings::canvas().size,
-         simulationGrid)
-      ),
-   simulationGrid(simulationGrid)
+                       Settings::engines::EnginesTypes::glyphSlices)
 {
    unNormalizedAlpha = Settings::sim::Scalar::fluidVelocityMagnitude;
 
    connectToSettings();
-
-   emit cellSizeChanged(dynamic_cast<UniformGrid *>(visualizationGrid)->getCellSize());
 }
 
 void GlyphSliceEngine::updateCache()
 {
-   TranslucentGlyphBuilder builder = TranslucentGlyphBuilder(
-                visualizationGrid,
-                Settings::visualization::glyphs().glyph,
-                Settings::visualization::glyphs().colorMap->textureGetter,
-                Settings::visualization::glyphs().vectorGetter,
-                Vertex::getScalarGetter(unNormalizedAlpha),
-                Settings::simulation().getRange(unNormalizedAlpha));
+   int idx = SimulationHistory::instance().mostRecentStateIdx();
+
+   TranslucentGlyphBuilder builder(
+      &SimulationHistory::instance().getVisualizationGridAtQueueIdx(idx),
+      Settings::visualization::glyphs().glyph,
+      Settings::visualization::glyphs().colorMap->textureGetter,
+      Settings::visualization::glyphs().vectorGetter,
+      Vertex::getScalarGetter(unNormalizedAlpha),
+      Settings::simulation().getRange(unNormalizedAlpha));
 
    GPUData data = builder.getGPUData();
    data.transform(toSliceTransformation);
@@ -39,26 +33,6 @@ void GlyphSliceEngine::updateCache()
 
 void GlyphSliceEngine::connectToSettings()
 {
-   connect(&Settings::simulation(), SIGNAL(recomputeVertexPositions(QSize,QSizeF)),
-            this, SLOT(onRecomputeVertexPositions(QSize,QSizeF)));
-   connect(this, SIGNAL(cellSizeChanged(QSizeF)),
-            &Settings::visualization::glyphs(), SLOT(onCellSizeChanged(QSizeF)));
-   connect(&Settings::visualization::glyphs(), SIGNAL(gridDimensionChanged(QSizeF)),
-           this, SLOT(onGridDimensionChanged(QSizeF)));
    connect(&Settings::visualization::glyphs(), SIGNAL(clearCache()),
             this, SLOT(onClearCache()));
-}
-
-void GlyphSliceEngine::onRecomputeVertexPositions(QSize canvasSize, QSizeF cellSize)
-{
-   visualizationGrid->changeGridArea(canvasSize, cellSize);
-   emit cellSizeChanged(dynamic_cast<UniformGrid *>(visualizationGrid)->getCellSize());
-   clearCache();
-}
-
-void GlyphSliceEngine::onGridDimensionChanged(QSizeF size)
-{
-   visualizationGrid = JitterGrid::createVisualizationGrid(size.width(), Settings::canvas().size, simulationGrid);
-   emit cellSizeChanged(dynamic_cast<UniformGrid *>(visualizationGrid)->getCellSize());
-   clearCache();
 }
