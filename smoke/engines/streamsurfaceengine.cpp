6f22eed3cd4392be5fdd116fc8f099366a5db243
Laura Baakman <laurabaakman@gmail.com>
2017-04-13 19:59:58 +0200

    Use the split.

--- smoke/engines/streamsurfaceengine.cpp
+++ smoke/engines/streamsurfaceengine.cpp
@@ -8,7 +8,7 @@ StreamSurfaceEngine::StreamSurfaceEngine(SimulationGrid *grid) :
 
 void StreamSurfaceEngine::draw()
 {
-   streamobject::Surface surface = streamobject::Surface::debugSurfaceWithHole(0);
+   streamobject::Surface surface = streamobject::Surface::debugSurfaceWithSplit(2);
    drawSurface(surface);
    if (Settings::visualization::streamSurfaces().showLines) drawLines(surface);
    if (Settings::visualization::streamSurfaces().showVertices) drawVertices(surface);
