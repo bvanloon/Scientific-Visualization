659ccbc3355c87714f8601852a9ee0781b5d10d3
Laura Baakman <laurabaakman@gmail.com>
2017-04-21 09:43:29 +0200

    We are not interested in changes in magnitude in the streakline.

--- smoke/visualizationbuilders/streaklinebuilder.cpp
+++ smoke/visualizationbuilders/streaklinebuilder.cpp
@@ -13,3 +13,8 @@ QVector3D StreakLineBuilder::integrate(QVector3D previousPosition)
    QVector3D nextPosition = previousPosition + QVector3D(10, 10, 10);
    return nextPosition;
 }
+
+bool StreakLineBuilder::isMagnitudeLargeEnough(float magnitude)
+{
+   return true;
+}
