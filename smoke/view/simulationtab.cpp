d1a213cd002fb315fd6a6325ed5e6e39bc896ef1
Laura Baakman <laurabaakman@gmail.com>
2017-04-21 16:30:18 +0200

    Don't crash if the engine is unknown.

--- smoke/view/simulationtab.cpp
+++ smoke/view/simulationtab.cpp
@@ -24,6 +24,8 @@ SimulationSettingPane::~SimulationSettingPane()
 void SimulationSettingPane::onEngineToggled(Settings::engines::EnginesTypes engine, bool checked)
 {
    EngineMap::const_iterator toggledPair = this->engines.find(engine);
+   if (toggledPair == this->engines.end()) return;
+
    if (toggledPair.value() != nullptr) toggledPair.value()->setChecked(checked);
    if (checked && this->is3DEngine(engine)) toggle2DEngines(false);
    if (checked && this->is2DEngine(engine)) toggle3DEngines(false);
