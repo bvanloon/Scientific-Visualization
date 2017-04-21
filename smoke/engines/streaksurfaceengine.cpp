3f16e751ba2f98fc9e8a9530196a631d0d67967f
Laura Baakman <laurabaakman@gmail.com>
2017-04-21 12:54:29 +0200

    Compute the surface in drawSurface.

--- smoke/engines/streaksurfaceengine.cpp
+++ smoke/engines/streaksurfaceengine.cpp
@@ -52,6 +52,14 @@ void StreakSurfaceEngine::drawLines(streamobject::Surface surface)
 
 void StreakSurfaceEngine::drawSurface(streamobject::Surface surface)
 {
+   static bool warningShown = false;
+   if (!warningShown++) qDebug() << "StreakSurfaceEngine::drawLines: Temporarily recomputing the streak lines.";
+
+   streamobject::Surface surface = streamobject::Surface(Settings::visualization::streakSurface().seedCurve,
+                                                             Settings::visualization::streakSurface().resolution);
+
+
+
    GPUData data = surface.GPUDataSurface();
    updateBuffersAndDraw(data);
 }
